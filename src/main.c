cc8b5901841c73c773dabb093ca7ffd525278108
Ryan ODell <ryanodell2013@gmail.com>
2024-05-18 10:04:57 -0700

    Added cmd line parsing

--- src/main.c
+++ src/main.c
@@ -1,7 +1,57 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main () {
-    printf("Test\n");
+void print_usage(const char *program_name) {
+    printf("Usage: %s [options]\n", program_name);
+    printf("Options:\n");
+    printf("  -i <input_file>  Specify input file\n");
+    printf("  -o <output_file> Specify output file\n");
+    printf("  -h               Display this help message\n");
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        print_usage(argv[0]);
+        return 1;
+    }
+
+    char *input_file = NULL;
+    char *output_file = NULL;
+
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-i") == 0) {
+            if (i + 1 < argc) {
+                input_file = argv[i + 1];
+                i++;
+            } else {
+                fprintf(stderr, "Error: -i option requires a value\n");
+                return 1;
+            }
+        } else if (strcmp(argv[i], "-o") == 0) {
+            if (i + 1 < argc) {
+                output_file = argv[i + 1];
+                i++;
+            } else {
+                fprintf(stderr, "Error: -o option requires a value\n");
+                return 1;
+            }
+        } else if (strcmp(argv[i], "-h") == 0) {
+            print_usage(argv[0]);
+            return 0;
+        } else {
+            fprintf(stderr, "Error: Unknown option %s\n", argv[i]);
+            print_usage(argv[0]);
+            return 1;
+        }
+    }
+
+    if (input_file) {
+        printf("Input file: %s\n", input_file);
+    }
+    if (output_file) {
+        printf("Output file: %s\n", output_file);
+    }
+    
     return 0;
-
 }
